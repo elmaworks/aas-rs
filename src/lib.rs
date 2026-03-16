6faeacd510c7d4b6111d18f239b2f0bbcebe5e16
elmaworks <ipnetwork82@gmail.com>
2026-03-16 11:49:10 +0900

    feat: init rust project and structure

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+#![allow(unused_imports)]
+
+pub mod common;
+pub mod constants;
+pub mod jsonization;
+pub mod stringification;
+pub mod types;
+pub mod verification;
+
+pub use constants::*;
+pub use types::*;
